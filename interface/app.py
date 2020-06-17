faebf41e8624aa5e5fedc623493e511491686ed0
pietro.canu <pietro.canu@he-arc.ch>
2020-06-17 11:24:34 +0200

    Utilisation du modèle (EN COURS)

--- interface/app.py
+++ interface/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
 import sys
+import subprocess
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app.config.update(
     CONFIG_PATH=os.path.join(basedir,'aiaccessibility', 'detectron2', 'configs'),
     MODEL_WEIGHTS=os.path.join(basedir,'aiaccessibility', 'models', 'MaskRCNN_Resnet101_FPN_3X'),
     OUTPUT_PATH=os.path.join(basedir, 'static', 'output'),
-    
+
     DROPZONE_ALLOWED_FILE_TYPE='image',
     DROPZONE_MAX_FILE_SIZE=1,
     DROPZONE_MAX_FILES=1,
@@ -44,9 +45,15 @@ def analyse():
         if request.files:
             image = request.files["file"]   
             filename = image.filename
-			
-			#subprocess.call([os.path.join(app.config['MODELE_PATH'],'demo.py'), os.path.join(app.config['CONFIG_PATH'], DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml), image, os.path.join(app.config['OUTPUT_PATH'], filename), os.path.join(app.config['MODEL_WEIGHTS'], 'model_final_trimmed.pth'), Model_Device])
-			
+
+            #subprocess.call([os.path.join(app.config['MODELE_PATH'],'demo.py'), os.path.join(app.config['CONFIG_PATH'], 'DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml'), image, os.path.join(app.config['OUTPUT_PATH'], filename), os.path.join(app.config['MODEL_WEIGHTS'], 'model_final_trimmed.pth'), Model_Device])
+            #execFile(os.path.join(app.config['MODELE_PATH'],'demo.py')['--config-file': os.path.join(app.config['CONFIG_PATH'],'DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml'), '--input': image, '--output': os.path.join(app.config['OUTPUT_PATH'], filename), 'MODEL.WEIGHTS': os.path.join(app.config['MODEL_WEIGHTS'], 'model_final_trimmed.pth'), 'MODEL.DEVICE': Model_Device])
+            #execFile(os.path.join(app.config['MODELE_PATH'],'demo.py')['config-file': os.path.join(app.config['CONFIG_PATH'],'DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml'), 'input': image, 'output': os.path.join(app.config['OUTPUT_PATH'], filename), 'opts':['MODEL.WEIGHTS': os.path.join(app.config['MODEL_WEIGHTS'], 'model_final_trimme
+            
+            #subprocess.call(os.path.join(basedir, 'demos.py')['config-file': os.path.join(app.config['CONFIG_PATH'],'DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml'), 'input': image, 'output': os.path.join(app.config['OUTPUT_PATH'], filename), 'MODEL.WEIGHTS': os.path.join(app.config['MODEL_WEIGHTS'], 'model_final_trimmed.pth'), 'MODEL.DEVICE': Model_Device])
+
+            #subprocess.call(os.path.join(basedir, 'demos.py'),os.path.join(app.config['CONFIG_PATH'],'DLA_mask_rcnn_X_101_32x8d_FPN_3x.yaml'), 'input': image, os.path.join(app.config['OUTPUT_PATH'], filename), os.path.join(app.config['MODEL_WEIGHTS'], 'model_final_trimmed.pth'), Model_Device)
+ 
             image.save(os.path.join(app.config["OUTPUT_PATH"], filename))
     return render_template('index.html', filename = setsrc(filename))
 
@@ -59,4 +66,3 @@ def cancel():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- interface/modeles.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-def get_parser():
-    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin models")
-    parser.add_argument("--input", nargs="+", help="A list of space separated input images")
-
-    return parser
-    
